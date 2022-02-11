9ace3dcb60504666e9eea96290eec98d378f007b
yangzhanping <yangzhanping123@qq.com>
2022-02-11 13:33:35 +0800

    完善Base

--- app/src/main/java/com/doit/kotlin/activtiy/AppActivity.kt
+++ app/src/main/java/com/doit/kotlin/activtiy/AppActivity.kt
@@ -3,9 +3,9 @@ package com.doit.kotlin.activtiy
 import android.os.Bundle
 import com.doit.base.activity.BaseActivity
 import com.doit.kotlin.R
-import com.doit.kotlin.databinding.ActivityMainBinding
+import com.doit.kotlin.databinding.AppActivityMainBinding
 
-class MainActivity : BaseActivity<ActivityMainBinding>() {
+class AppActivity : BaseActivity<AppActivityMainBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +18,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     }
 
     override fun getLayoutId(): Int {
-        return R.layout.activity_main;
+        return R.layout.app_activity_main;
     }
 }
